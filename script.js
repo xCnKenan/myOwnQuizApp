d12c934fab71e4f3fda5779b80ab0977e8665d5d
xCnKenan <cn.kenan@icloud.com>
2025-01-19 22:22:28 +0100

    add json array

---
+++ script.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+        "question":"Welche der folgenden Programmiersprachen ist für Anfänger am einfachsten zu lernen?",
+        "answer_1":"Assembly",
+        "answer_2":"Python",
+        "answer_3":"C++",
+        "answer_4":"Java",
+        "right_answer": 2
+    },
+    {
+        "question":"Was ist eine Variable in der Programmierung?",
+        "answer_1":"Ein Wert, der sich niemals ändert",
+        "answer_2":"Eine Sammlung von Daten",
+        "answer_3":"Ein Container für Datenwerte",
+        "answer_4":"Ein spezielles Keyword",
+        "right_answer": 3
+    },
+    {
+        "question":"Welche Programmiersprache wird hauptsächlich für die Webentwicklung im Frontend verwendet?",
+        "answer_1":"Python",
+        "answer_2":"JavaScript",
+        "answer_3":"C++",
+        "answer_4":"PHP",
+        "right_answer": 2
+    },
+    {
+        "question":"Wie wird eine Schleife genannt, die eine festgelegte Anzahl von Wiederholungen ausführt?",
+        "answer_1":"While-Schleife",
+        "answer_2":"Do-While-Schleife",
+        "answer_3":"For-Schleife",
+        "answer_4":"Endlosschleife",
+        "right_answer": 3
+    },
+    {
+        "question":"Welche Dateiendung wird üblicherweise für JavaScript-Skripte verwendet?",
+        "answer_1":".js",
+        "answer_2":".py",
+        "answer_3":".cpp",
+        "answer_4":".html",
+        "right_answer": 1
+    },
+    {
+        "question":"Was ist HTML in der Programmierung?",
+        "answer_1":"Eine Programmiersprache",
+        "answer_2":"Ein Texteditor",
+        "answer_3":"Eine Auszeichnungssprache",
+        "answer_4":"Ein Datenbankformat",
+        "right_answer": 3
+    },
+    {
+        "question":"Welche Funktion gibt die Länge einer Liste in JavaScript zurück?",
+        "answer_1":"length()",
+        "answer_2":"count()",
+        "answer_3":"size()",
+        "answer_4":"len()",
+        "right_answer":1
+    }
+];
