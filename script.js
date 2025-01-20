070861046fdc99c3218014521700480fe2cfea14
xCnKenan <cn.kenan@icloud.com>
2025-01-20 18:56:21 +0100

    added answers

--- script.js
+++ script.js
@@ -56,3 +56,38 @@ let questions = [
         "right_answer":1
     }
 ];
+
+let currentQuestion = 0;
+
+function init(){
+    document.getElementById("all-questions").innerHTML = questions.length;
+
+    showQuestion();
+}
+
+function showQuestion(){
+    let question = questions[currentQuestion];
+
+    document.getElementById("questionText").innerHTML = question['question'];
+
+    document.getElementById("answer_1").innerHTML = question['answer_1'];
+    document.getElementById("answer_2").innerHTML = question['answer_2'];
+    document.getElementById("answer_3").innerHTML = question['answer_3'];
+    document.getElementById("answer_4").innerHTML = question['answer_4'];
+}
+
+function answer(selection){
+    let question = questions[currentQuestion];
+    console.log('Selected answer is', selection);
+    let selectedQuestionNumber = selection.slice(-1);
+    console.log('selectedQuestionNumber is', selectedQuestionNumber);
+    
+    console.log('Current question is', question['right_answer']);
+    
+    if (selectedQuestionNumber == question['right_answer'] ){
+        console.log("richtige Antwort!!");
+    } else{
+        console.log("falsche Antwort!!");
+        
+    }
+}
