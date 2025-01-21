fe0b58f59eb3d743cffef18c579d2a62585386aa
xCnKenan <cn.kenan@icloud.com>
2025-01-21 15:04:45 +0100

    add sound & complete

--- script.js
+++ script.js
@@ -64,6 +64,9 @@ let questions = [
 
 let currentQuestion = 0;
 let rightQuestions = 0;
+let AUDIO_SUCCES = new Audio("sounds/rightanswer.mp3");
+let AUDIO_FAIL = new Audio("sounds/wrong.mp3");
+let AUDIO_COMPLETE = new Audio("sounds/win.mp3");
 
 function init() {
   document.getElementById("all-questions").innerHTML = questions.length;
@@ -71,34 +74,48 @@ function init() {
 }
 
 function showQuestion() {
+  if (currentQuestion >= questions.length) {
+    // Show End Screen
+    document.getElementById("endScreen").style = "";
+    document.getElementById("questionBody").style = "display:none";
+    document.getElementById("amount-of-questions").innerHTML = questions.length;
+    document.getElementById("amount-of-right-questions").innerHTML =
+      rightQuestions;
+    document.getElementById("header-img").src = "/assets/img/trophy.png";
+    AUDIO_COMPLETE.play();
+  } else {
+    //Show question
+    let percent = Math.round(((currentQuestion + 1) / questions.length) * 100);
+    document.getElementById("progress-bar").innerHTML = `${percent} %`;
+    document.getElementById("progress-bar").style = `width: ${percent}%`;
 
-if(currentQuestion >= questions.length){
-    // TODO: Show End Screen
-    document.getElementById('endScreen').style='';
-    document.getElementById('questionBody').style= 'display:none';
-    document.getElementById('amount-of-questions').innerHTML = questions.length;
-    document.getElementById('amount-of-right-questions').innerHTML = rightQuestions;
-}else {
-  let question = questions[currentQuestion];
-  document.getElementById("questionText").innerHTML = question["question"];
-  document.getElementById("answer_1").innerHTML = question["answer_1"];
-  document.getElementById("answer_2").innerHTML = question["answer_2"];
-  document.getElementById("answer_3").innerHTML = question["answer_3"];
-  document.getElementById("answer_4").innerHTML = question["answer_4"];
-  document.getElementById("current-question").innerHTML = `${currentQuestion + 1}`;
-    }
+    let question = questions[currentQuestion];
+    document.getElementById("questionText").innerHTML = question["question"];
+    document.getElementById("answer_1").innerHTML = question["answer_1"];
+    document.getElementById("answer_2").innerHTML = question["answer_2"];
+    document.getElementById("answer_3").innerHTML = question["answer_3"];
+    document.getElementById("answer_4").innerHTML = question["answer_4"];
+    document.getElementById("current-question").innerHTML = `${
+      currentQuestion + 1
+    }`;
+  }
 }
 
 function answer(selection) {
   let question = questions[currentQuestion];
   let selectedQuestionNumber = selection.slice(-1);
   let idOfRightAnswer = `answer_${question["right_answer"]}`;
-  if (selectedQuestionNumber == question["right_answer"]) { //richtige Frage beantwortet
+  if (selectedQuestionNumber == question["right_answer"]) {
+    //richtige Frage beantwortet
     document.getElementById(selection).parentNode.classList.add("bg-success"); // durch parentNode wird es dem übergeordneten div zugeordnet
+    AUDIO_SUCCES.play();
     rightQuestions++;
   } else {
     document.getElementById(selection).parentNode.classList.add("bg-danger");
-    document.getElementById(idOfRightAnswer).parentNode.classList.add("bg-success");
+    document
+      .getElementById(idOfRightAnswer)
+      .parentNode.classList.add("bg-success");
+    AUDIO_FAIL.play();
   }
   document.getElementById("next-button").disabled = false;
 }
@@ -119,4 +136,13 @@ function resetAnswers() {
   document.getElementById("answer_3").parentNode.classList.remove("bg-danger");
   document.getElementById("answer_4").parentNode.classList.remove("bg-success");
   document.getElementById("answer_4").parentNode.classList.remove("bg-danger");
+}
+
+function restartGame() {
+  document.getElementById("header-img").src = "/assets/img/quizlogo.jpg";
+  document.getElementById("endScreen").style = "display:none"; // Endscreen ausblenden
+  document.getElementById("questionBody").style = ""; // questionbody wieder anzeigen
+  currentQuestion = 0;
+  rightQuestions = 0;
+  init();
 }
